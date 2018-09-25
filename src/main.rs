cd5784b21f957689544e5345d5799bdffc5d19cd
Maid Dog <maiddogsrl@gmail.com>
2018-09-24 23:12:25 -0500

    Not working, trying out pest

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use lalrpop_util::lalrpop_mod;
 
+pub mod ast;
 lalrpop_mod!(pub math);
 
 fn main() {
-    let math = math::ExprParser::new();
-    println!("{:?}", math.parse("-22 + 3 * (4^-2 + 1)"));
+    let math = math::ProgramParser::new();
+    println!("{:?}", math.parse("label_:\n-22 + 3 * (4^-2 + 1)"));
     println!("{:?}", math.parse("$20 - %0110"));
 }
